6b7b6801de649c61558b4aa8fbad5e4df1b437cb
bpncodes <131p11v@gmail.com>
2020-09-10 17:41:05 +0530

    added manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-top-layout',
+    "gatsby-plugin-top-layout",
     {
-      resolve: 'gatsby-plugin-material-ui',
+      resolve: "gatsby-plugin-material-ui",
       // If you want to use styled components you should change the injection order.
       options: {
         // stylesProvider: {
@@ -12,9 +12,21 @@ module.exports = {
     },
     // If you want to use styled components you should add the plugin here.
     // 'gatsby-plugin-styled-components',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/public/apple-touch-icon.png`, // This path is relative to the root of the site.
+      },
+    },
   ],
   siteMetadata: {
-    title: 'My page',
+    title: "My page",
   },
 };
